454d643750978f45bd1500d45fc93e20b654c54d
Lexaloris <Lexa-loris@mail.ru>
2014-11-20 19:54:16 +0400

    a little bit improved

---
+++ src/test/java/frontend/LogoutServletTest.java
@@ -0,0 +1,19 @@
+package frontend;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogoutServletTest extends TestCase {
+
+    @Test
+    public void testDoGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDoPost() throws Exception {
+
+    }
+}
